be7e6db577d37982e0b08f92e369a90cee75128c
Shreyas <shreyas.gaonkar@yahoo.com>
2015-07-11 17:56:20 +0530

    removed vScroll.js function
    used initially for testing purposes

--- includes/js/script.js
+++ includes/js/script.js
@@ -2,7 +2,7 @@
 $(window).scroll(function () {
 
     var wScroll = $(this).scrollTop();
-    console.log(wScroll);
+    
     $('.mote-landing-page').css({
         'transform' : 'translate(0px, '+ wScroll /3 +'%)'
     });
